155c5011051140da121c4f727cc9d05268e2bf3a
BrendanBD <brendanbdal@gmail.com>
2023-10-31 10:53:51 -0400

    Finished exam Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Multiple from "./Form"; // Assuming "form.js" is in the same directory as "App.js"
+import Multiple from "./Form"; 
 import "./App.css";
 function App() {
   return (
